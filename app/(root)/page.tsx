e9011ec922a6003d44242bcf087e3ca64c868291
labae <onetyten@gmail.com>
2025-03-11 01:34:48 +0100

    Homepage init

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image"
-import Welcome from "@/app/welcome";
-
-export default function Home() {
+ export default function Home() {
   console.log("This is a server component")
   return (
     <div className=" flex flex-col gap-5 items-center justify-center min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
-      <h2 className="text-2xl">
+      <h2 className="text-2xl font-inter font-extralight">
         Hello there
-      </h2>
-      <Welcome/>
-
-      
-     
+      </h2>   
     </div>
   );
 }
+  
---
+++ app/components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='px-5 py-3'>Navbar</div>
+  )
+}
+
+export default Navbar
