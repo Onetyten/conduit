febb50e76ceb21699df19f56c814187acf48e727
labae <onetyten@gmail.com>
2025-03-23 18:18:17 +0100

    Talent profile

--- app/(root)/(profile)/profile/page.tsx
+++ app/(root)/(profile)/profile/page.tsx
@@ -33,31 +33,64 @@ const page = () => {
         </div>
   )
   return (
-    <div className='w-full h-screen flex relative'>
+    <div className='w-full min-h-screen flex relative'>
       <Link href={'/'} className='absolute top-3 left-3 text-2xl bg-blue-100 hover:bg-blue-200 p-2 text-conduit rounded-full'>
         <MdArrowBack className=''/>
       </Link>
       
-      <div className='bg-softblue flex-1 flex justify-center items-center'>
+      <div className='bg-softblue flex-1 p-5 flex justify-center items-center'>
          <Avatar className='w-56 h-56'>
             <AvatarImage src ={userReduxData?.profilePicture}/>
             <AvatarFallback>{`${userReduxData?.lastName.slice(0,1)}${userReduxData?.firstName.slice(0,1)}`}</AvatarFallback>
           </Avatar>
-
       </div>
-      <div className='flex-1 flex flex-col justify-center items-center gap-5'>
+      <div className='flex-1 flex flex-col p-5 justify-center items-center gap-5'>
         <div className='flex flex-col p-10 gap-5 text-xs '>
           <p className='text-3xl font-semibold'>{`${userReduxData?.lastName} ${userReduxData?.firstName}`}</p>
           <div className='text-sm flex gap-3'>
             <p className='p-1.5 px-4 hover:bg-blue-100 bg-softblue rounded-md'>
               {userReduxData?.isTalent?'Talent':'Client'}
             </p>
-           
+            {userReduxData?.serviceCategories.length>0&&(
+              <p className='p-1.5 px-4 hover:bg-blue-100 bg-softblue rounded-md'>
+              {userReduxData?.serviceCategories.length>0 ?userReduxData?.serviceCategories:'Client'}
+            </p> 
+            )}
+            
           </div>
-          <p className='text-sm py-10'>{`${userReduxData?.bio}`}</p>
+          {userReduxData?.skills && userReduxData?.skills.length > 0 && (
+            <div className='text-sm gap-2 flex flex-col'>
+              <p className='font-semibold'>Skills </p>
+              <div className='flex flex-wrap gap-3'> 
+                {userReduxData?.skills.map((item, index) => (
+                  <span key={index} className='p-1.5 px-4 hover:bg-blue-100 bg-softblue rounded-md'>{item}</span>
+                ))}
+              </div>
+             
+            </div>
+          )}
+
+
+
+          <p className='text-sm py-3'>{`${userReduxData?.bio}`}</p>
+
+          {userReduxData.companyName&&(
+            <div className='flex flex-col gap-3'>
+              <p className='font-semibold text-sm'>Company Details</p>
+              <div className='flex flex-col p-2 gap-3 w-full border-softblue ml-3 border-2 text-sm rounded-lg'>
+                <p className='font-semibold text-base'>{`${userReduxData?.companyName}`}</p>
+                <p className=''>{`${userReduxData?.companyDescription}`}</p>
+                <p className=''>{`Hourly rate : ${userReduxData?.hourlyRate} USD`}</p>
+                <Link href={userReduxData.portfolio} className='text-blue-600'>
+                  {userReduxData.portfolio}
+                </Link>
+              </div>
+            </div>
+          )}
+
           <p className=''>{`${userReduxData?.email}`}</p>
           <p>{`${userReduxData?.location?.city?userReduxData?.location?.city:''} ${userReduxData.location.state?userReduxData.location.state:''}, ${userReduxData.location.country?userReduxData.location.country:''}`}</p>
-          {/* <p className='text-sm'>{`${userReduxData.totalMoneySpent}`}</p> */}
+          
           <p className=''>{`${userReduxData?.isTalent?'Talent':'Client'} since ${formattedCreationDate}`}</p>
           <div className='text-sm bg-red-500 w-28 text-center p-3 my-5 hover:bg-red-600 text-white rounded-lg' onClick={signOut}>
             Sign out
