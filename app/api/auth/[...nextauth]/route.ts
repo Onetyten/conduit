1c59002b3b188249f656285c2eff4f30003943ba
Onetyten <onetyten@gmail.com>
2025-07-05 15:24:03 +0100

    next auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,15 @@
-// app/api/auth/[...nextauth]/route.ts (or route.js)
-import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+// // app/api/auth/[...nextauth]/route.ts (or route.js)
+// import NextAuth from "next-auth";
+// import GithubProvider from "next-auth/providers/github";
 
-export const {
-  handlers: { GET, POST },
-  auth,
-} = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.AUTH_GITHUB_ID,
-      clientSecret: process.env.AUTH_GITHUB_SECRET,
-    }),
-  ],
-});
+// export const {
+//   handlers: { GET, POST },
+//   auth,
+// } = NextAuth({
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.AUTH_GITHUB_ID,
+//       clientSecret: process.env.AUTH_GITHUB_SECRET,
+//     }),
+//   ],
+// });
