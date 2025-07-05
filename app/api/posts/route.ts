6eec3dcf15d19b0ee99fc28e20ea2715805859d5
Onetyten <onetyten@gmail.com>
2025-07-05 17:51:05 +0100

    post string

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -6,21 +6,25 @@ const DEFAULT_LIMIT = 10
 
 export async function GET(request:Request) {
     const {searchParams} = new URL(request.url)
-    const page = parseInt(searchParams.get('page')) || 1
-    const limit = parseInt(searchParams.get('limit')) || DEFAULT_LIMIT
+    const page = Number(searchParams.get('page')) || 1
+    const limit = Number(searchParams.get('limit')) || DEFAULT_LIMIT
     const skip = (page-1)*limit
     try {
         await mongoConnect()
         const totalPosts = await Service.countDocuments()
-        const Post  = await Service.find({}).skip(skip).limit(limit).sort({createdAt:-1}).exec()
+        const posts  = await Service.find({}).skip(skip).limit(limit).sort({createdAt:-1}).exec()
         const totalPages = Math.ceil(totalPosts/limit)
         const hasMore = totalPages>page
-        return NextResponse.json({ message:"Posts retrieved Successfully",posts:Post,currentPage:page,totalPages,hasMore},{status:200})
+        return NextResponse.json({ message:"Posts retrieved Successfully",posts,currentPage:page,totalPages,hasMore},{status:200})
 
 
     } catch (error) {
         console.error(error)
-        return NextResponse.json({error:error.message},{status:500})
+        let errorMessage = "An unknown error occurred";
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+        return NextResponse.json({error: errorMessage},{status:500})
     }
     
 }
