51ef74ffcc9a2a019a6bdba393f02158cf0eaf22
onetyten <labaekaabdulrazaq@gmail.com>
2026-03-02 03:01:47 +0100

    npm update

--- app/(root)/(Home)/page.tsx
+++ app/(root)/(Home)/page.tsx
@@ -1,7 +1,6 @@
 
 import SectionCarousel from "@/components/sectionCarousel";
 import IntroSection from "@/components/introSection";
-import SearchForm from "@/components/searchForm"
 import Posts from "@/components/posts";
 import ServiceSlider from "@/components/serviceSlider";
 import CreateAccount from "@/components/createAccount";
@@ -14,7 +13,7 @@ export default function Home() {
     <div className="flex flex-col relative raleway-text w-[90%] sm:w-xl md:w-2xl lg:w-4xl  xl:w-5xl 2xl:w-7xl justify-center items-center">
       
       <IntroSection />
-      <SearchForm />
+      
       <SectionCarousel />
       <div className="w-full">  
           <SearchMessage/>
--- components/introSection.tsx
+++ components/introSection.tsx
@@ -1,51 +1,64 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import HeaderSlide from './headerSlide'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '@/store'
 import Link from 'next/link'
 import { signUpTrue } from '@/state/showSignUp/showSignUp'
+import SearchForm from './searchForm'
 
 const IntroSection = () => {
     const dispatch = useDispatch()
+
+    const [hiring,setHiring] = useState(true)
+
     const userReduxData = useSelector((state:RootState)=>state.user.user)
   return (
-    <div className='w-full flex sm:flex-row flex-col justify-center gap-3 items-center py-10 px-2'>
-        <div className=' py-10 gap-6 flex flex-col w-full sm:w-md'>
-            <h1 className='text-3xl font-medium'>Skills on Demand</h1>
-            <p className='text-sm text-gray-500'>Conduit is revolutionizing service hiring by creating a seamless marketplace for connecting clients with skilled professionals.  </p>
+    <div className='flex flex-col justify-center items-center py-10 px-2'>
+        
+        <div className='bg-gray-200 text-sm font-bold uppercase p-2 rounded-md '>
+            <span className='p-4 cursor-pointer'>Hire</span>
+            <span className='p-4 cursor-pointer'>Get Hired</span>
+        </div>
 
-            <div className='sm:text-sm text-xs flex gap-3'>
+     
+        <div className='w-full flex sm:flex-row flex-col justify-center gap-3 items-center '>
+            <div className=' py-10 gap-6 flex flex-col w-full sm:w-md'>
+                <h1 className='text-3xl font-medium'>Skills on Demand</h1>
+                <p className='text-sm text-gray-500'>Conduit is revolutionizing service hiring by creating a seamless marketplace for connecting clients with skilled professionals.  </p>
 
-                <div className='hover:text-background  hover:bg-foreground cursor-pointer text-foreground border-[1px] border-foreground transition-all  w-fit px-5 py-2 rounded-full'>
-                    {userReduxData?(
-                        <div>
-                          {userReduxData?.isTalent?
-                            (
-                            <Link href="/" className='cursor-pointer' >
-                                Get Hired
-                            </Link>
-                            ):(
-                            <button onClick={()=>{dispatch(signUpTrue())}} className='cursor-pointer' >
-                                Get Hired
-                            </button>
-                            )}  
-                        </div>
-                    
-                    ):(
-                    <button onClick={()=>{dispatch(signUpTrue())}} className='cursor-pointer' >
-                      Get Hired
-                    </button>
-                    )}
+                <div className='sm:text-sm text-xs flex gap-3'>
+
+                    <div className='hover:text-background  hover:bg-foreground cursor-pointer text-foreground border-[1px] border-foreground transition-all  w-fit px-5 py-2 rounded-full'>
+                        {userReduxData?(
+                            <div>
+                            {userReduxData?.isTalent?
+                                (
+                                <Link href="/" className='cursor-pointer' >
+                                    Get Hired
+                                </Link>
+                                ):(
+                                <button onClick={()=>{dispatch(signUpTrue())}} className='cursor-pointer' >
+                                    Get Hired
+                                </button>
+                                )}  
+                            </div>
+                        
+                        ):(
+                        <button onClick={()=>{dispatch(signUpTrue())}} className='cursor-pointer' >
+                            Get Hired
+                        </button>
+                        )}
+                    </div>
                 </div>
             </div>
+
+            <div className='w-full sm:w-md'>
+                <HeaderSlide/>
+
+            </div>
         </div>
-
-        <div className='w-full sm:w-md'>
-            <HeaderSlide/>
-
-
-        </div>
+        <SearchForm/>
     </div>
   )
 }
